41ff0509349a4a0325aa053cb713cef47b506de6
AmrAbdelgwaad <115653887+AmrAbdelgwaad@users.noreply.github.com>
2023-09-09 01:29:12 +0300

    a little update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const thankingContainer = document.querySelector(".container");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const rating = document.querySelector("input[type= 'radio']:checked").value;
-  const para = document.querySelector(".thanks__rating");
+  const para = thankingContainer.querySelector(".thanks__rating");
   para.textContent = `You selected ${rating} out of 5`;
   ratingContainer.classList.add("hidden");
   thankingContainer.classList.remove("hidden");
